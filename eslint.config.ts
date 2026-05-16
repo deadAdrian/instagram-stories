e6497e237dbaf88226680a25d166cca6824c37eb
deadAdrian <0adrianvidal@gmail.com>
2026-05-16 19:39:59 -0300

    chore: setup eslint

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: {
+      js,
+      '@stylistic': stylistic,
+    },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+  }),
+]);
